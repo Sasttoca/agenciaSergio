2e3e211e98c44d15c5c527f964a7a1a8052599dc
Sasttoca <samueltovar504@gmail.com>
2026-05-23 02:19:27 -0500

    fix: rutas relativas en index.html y simplificación de vercel.json

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './' // Esto asegura que busque los archivos en la ruta relativa correcta
 })
